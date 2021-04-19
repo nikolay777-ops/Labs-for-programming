fcebc2d7417e8b67c0bbf7a7d25262f9d1ec548d
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-04-20 02:09:19 +0300

    Update Lab4.2Test.c

--- Lab4/Lab4.2/Lab4.2Test.c
+++ Lab4/Lab4.2/Lab4.2Test.c
@@ -7,32 +7,23 @@ static int size = 0;
 
 void FirstTest()
 {
-    char* str1;
-    char* str2;
-    char* str3;
+    char str1[] = "12345";
+    char str2[] = "1000000000";
+    char str3[] = "2147483647";
     unsigned long int temp = 0;
-    str1 = ("12345\0");
-    str2 = ("1000000000\0");
-    str3 = ("2147483647\0");
     temp = StoI(str1, StrSize(str1) - 1);
     assert(temp == 12345);
     temp = StoI(str2, StrSize(str2) - 1);
     assert(temp == 1000000000);
     temp = StoI(str3, StrSize(str3) - 1);
     assert(temp == 2147483647);
-    free(str1);
-    free(str2);
-    free(str3);
 }
 
 void SecondTest()
 {
-    char* str1;
-    char* str2;
-    char* str3;
-    str1 = ("Aspirin 15.04.99 132 1250\0");
-    str2 = ("$*(&(! 15.&*.$) &($) *()$!\0");
-    str3 = ("Aspirin $£!$ 132 1250\0");
+    char str1[] = "Aspirin 15.04.99 132 1250";
+    char str2[] = "$*(&(! 15.&*.$) &($) *()$!";
+    char str3[] = "Aspirin $£!$ 132 1250";
     assert(IsName(str1));
     assert(IsDate(str1));
     assert(IsStandards(str1));
@@ -42,25 +33,18 @@ void SecondTest()
     assert(!IsStandards(str2));
     assert(!IsPrice(str2));
     assert(!IsDate(str3));
-    free(str1);
-    free(str2);
-    free(str3);
 }
 
 void ThirdTest()
 {
     struct pills* test = (struct pills*)malloc(sizeof(struct pills) * 2);
     int temp;
-    char* str1;
-    char* str2;
-    str1 = ("Aspirin 15.04.99 125 1300\0");
-    str2 = ("Insulin 17.02.05 130 1250\0"); 
+    char str1 = "Aspirin 15.04.99 125 1300";
+    char str2 = "Insulin 17.02.05 130 1250"; 
     StrToStruct(test, str1);
     StrToStruct(test, str2);
     temp = OverDuePills(test);
     assert(temp == 3221440);
-    free(str1);
-    free(str2);
     free(test);
 }
 
