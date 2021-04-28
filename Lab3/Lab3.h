1912831ac6e817b0ead935044ee98f73abe293cc
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-04-29 01:38:38 +0300

    Delete Lab3.h

--- Lab3/Lab3.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-float **MatrixCreate(int* size);
-void FillingArray(float **arr, int *size);
-void FillingBackArray(float** arr, int* size);
-void Info(float **arr, int *size);
-void CountingBackMatrix(float **arr, float **backArr, int *size);
