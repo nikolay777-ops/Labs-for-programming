6604722cbd15b98f6ae39d2d7d0df94c243a596b
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-04-29 01:42:16 +0300

    Update Lab3Test.c

--- Lab3/Lab3Test.c
+++ Lab3/Lab3Test.c
@@ -4,12 +4,12 @@
 
 void FirstTest()
 {
-    float** matrix;
+    	float** matrix;
 	float** backMatrix;
 	int i, j, det;
 	int N;
-    srand(time(NULL));
-    N = 3;
+    	srand(time(NULL));
+    	N = 3;
 	matrix = MatrixCreate(N);
 	backMatrix = MatrixCreate(N);
 	matrix[0][0] = 1;
@@ -23,82 +23,82 @@ void FirstTest()
 	matrix[2][2] = 9;
 	det = Determinant(matrix, N);
 	assert(det == 0);
-    FillingBackArray(backMatrix, N);
-    for (i = 0; i < N; i++)
-    {
-        for(j = 0; j < N; j++)
-        {
-            matrix[i][j] = rand() % 100 - 50;
-        }
-    }
-    det = Determinant(matrix, N);
-    assert(det != 0);
-    for (i = 0; i < N; i++)
-    {
-        free(matrix[i]);
-        free(backMatrix[i]);
-    }
-}
+    	FillingBackArray(backMatrix, N);
+    	for (i = 0; i < N; i++)
+    	{
+        	for(j = 0; j < N; j++)
+        	{
+           	matrix[i][j] = rand() % 100 - 50;
+        	}
+    	}
+    	det = Determinant(matrix, N);
+    	assert(det != 0);
+    	for (i = 0; i < N; i++)
+    	{
+       		free(matrix[i]);
+        	free(backMatrix[i]);
+    	}
+	}
 
 void SecondTest()
 {
-    float** matrix;
+    	float** matrix;
 	float** backMatrix;
 	int N, i, j, sum, det;
 	srand(time(NULL));
-    sum = 0;
-    N = 3;
+    	sum = 0;
+    	N = 3;
 	matrix = MatrixCreate(N);
 	backMatrix = MatrixCreate(N);
-    FillingBackArray(backMatrix, N);
-    for (i = 0; i < N; i++)
-    {
-        for(j = 0; j < N; j++)
-        {
-            matrix[i][j] = rand() % 35;
-        }
-    }
-    det = Determinant(matrix,N);
-    assert(det != 0);
-    CountingBackMatrix(matrix, backMatrix, N);
-    for(i = 0, j = 0; i < N; i++, j++)
-    sum += matrix[i][j];
-    assert(sum == 3);
-    for (i = 0; i < N; i++)
-    {
-        free(matrix[i]);
-        free(backMatrix[i]);
-    }
+    	FillingBackArray(backMatrix, N);
+    	for (i = 0; i < N; i++)
+    	{
+        	for(j = 0; j < N; j++)
+        	{
+           	 matrix[i][j] = rand() % 35;
+        	}
+    	}
+	det = Determinant(matrix,N);
+	assert(det != 0);
+	CountingBackMatrix(matrix, backMatrix, N);
+	for(i = 0, j = 0; i < N; i++, j++)
+	sum += matrix[i][j];
+	assert(sum == 3);
+	for (i = 0; i < N; i++)
+	{
+		free(matrix[i]);
+		free(backMatrix[i]);
+	}
 }
 
 void ThirdTest()
 {
-    float** matrix;
+    	float** matrix;
 	float** backMatrix;
 	int N, i, j, sum, det;
 	srand(time(NULL));
-    sum = 0;
-    N = 6;
+    	sum = 0;
+    	N = 6;
 	matrix = MatrixCreate(N);
 	backMatrix = MatrixCreate(N);
-    FillingBackArray(backMatrix, N);
-    for (i = 0; i < N; i++)
-    {
-        for(j = 0; j < N; j++)
-        {
-            matrix[i][j] = rand() % 25;
-        }
-    }
-    det = Determinant(matrix, N);
-    CountingBackMatrix(matrix, backMatrix, N);
-    for(i = 0, j = 0; i < N; i++, j++)
-    sum += matrix[i][j];
-    assert(sum == 6);
-    for (i = 0; i < N; i++)
-    {
-        free(matrix[i]);
-        free(backMatrix[i]);
-    }
+	FillingBackArray(backMatrix, N);
+	for (i = 0; i < N; i++)
+	{
+		for(j = 0; j < N; j++)
+		{
+		    matrix[i][j] = rand() % 25;
+		}
+	}
+	det = Determinant(matrix, N);
+	CountingBackMatrix(matrix, backMatrix, N);
+	for(i = 0, j = 0; i < N; i++, j++)
+	sum += matrix[i][j];
+	assert(sum == 6);
+	for (i = 0; i < N; i++)
+    	{
+        	free(matrix[i]);
+        	free(backMatrix[i]);
+    	}
 }
 
 int main()
