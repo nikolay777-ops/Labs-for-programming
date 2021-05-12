8409cb518bc20459d6851a69d38b576c35f04277
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-05-13 00:29:22 +0300

    Add files via upload

---
+++ Lab6.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct QueueNode
+{
+	int value;
+	struct QueueNode *next;
+	struct QueueNode *prev;
+}QueueNode;
+
+typedef struct Queue
+{
+	QueueNode* head;
+	QueueNode* tail;
+	int size;
+}Queue;
+
+typedef struct TreeNode
+{
+	struct TreeNode* leftChild;
+	struct TreeNode* rightChild;
+	struct TreeNode* parent;
+	int data;
+}TreeNode;
+
+Queue* QueueCreate(Queue* q);
+
+void QInit(Queue** q);
+
+void QPush(struct Queue **q, int value);
+
+void QPop(Queue *q);
+
+void QClear(Queue *q);
+
+int QTop(Queue **q);
+
+void TreeInit(struct TreeNode** root);
+
+struct TreeNode* CreateNode(int value);
+
+void BuildTree(struct TreeNode** root, struct TreeNode** newNode);
+
+void GiveParent(struct TreeNode* node);
+
+void Print(struct TreeNode** root);
+
+void TraversePreorder(struct TreeNode** root);
+
+void TraversePostorder(struct TreeNode** root);
+
+void TClear(struct TreeNode **root);
