ee85d8cc470cabd67133421fb86f2dd743cbeb2c
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-06-01 01:26:05 +0300

    Update Test2.c

--- Lab7Test/Test2.c
+++ Lab7Test/Test2.c
@@ -8,10 +8,10 @@ void FirstTest()
     list->head = list->tail = NULL;
     char* str = (char*)malloc(sizeof(char) * 15);
     assert(str);
-	str = "Kanye West";
+    str = "Kanye West";
     Alb(&list, str);
-	assert(list != NULL);
-	assert(list->head != NULL);
+    assert(list != NULL);
+    assert(list->head != NULL);
     assert(list->tail != NULL);
     assert(strncmp(list->head->perfID, "Kan", 3) == 0);
     //assert(strncmp(list->head->next->next->perfID, "Kil", 3) == 0);
