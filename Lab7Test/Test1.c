d58e5f9a5195429f702a26acfb3a0ae69f00f12e
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-06-01 01:34:49 +0300

    Update Test1.c

--- Lab7Test/Test1.c
+++ Lab7Test/Test1.c
@@ -11,14 +11,36 @@ void PerfomancesTest()
     assert(list != NULL);
     assert(list->head != NULL);
     assert(list->tail != NULL);
-	SearchID(&list, "-HHP");
+    SearchID(&list, "-HHP");
     SearchName(&list, gen);
-   // PerfClear(&list);
+    PerfClear(&list);
     free(list);
+}
+
+void FirstTest()
+{
+    struct AlbList* list = NULL;
+    list = (struct AlbList*)malloc(sizeof(struct AlbList));
+    list->head = list->tail = NULL;
+    char* str = (char*)malloc(sizeof(char) * 15);
+    assert(str);  
+    str = "Kanye West";
+    Alb(&list, str);
+    str = (char*)malloc(sizeof(char) * 15);
+    str = "Killstation";
+    Alb(&list, str);
+    assert(list != NULL);
+    assert(list->head != NULL);
+    assert(list->tail != NULL);
+    assert(strncmp(list->head->perfID, "Kan", 3) == 0);
+    assert(strncmp(list->head->next->next->perfID, "Kil", 3) == 0);
+    //assert(strncmp(list->head->next->next->next->next->perfID, "Emi", 3) == 0);
+    //assert(strncmp(list->head->next->next->next->next->next->next->perfID, "Dav", 3) == 0);
 }
 
 int main()
 {
+    FirstTest();
     PerfomancesTest();
     return 0;
 }
