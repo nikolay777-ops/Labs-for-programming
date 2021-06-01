a3bb2e84b7a47f9d759b0a5c995f869b466368f5
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-06-01 03:38:18 +0300

    Update Test.c

--- Lab7Test/Test.c
+++ Lab7Test/Test.c
@@ -7,39 +7,37 @@ void UITest()
 {
     struct AlbList* list = NULL;
     struct UserList* usList = NULL;
-	char* str = (char*)malloc(sizeof(char) * 21);
-	assert(str != NULL);
-	AllAlbums(&list);
+    char* str = (char*)malloc(sizeof(char) * 21);
+    assert(str != NULL);
+    AllAlbums(&list);
     usList = (struct UserList*)malloc(sizeof(struct UserList));
-    assert(usList != NULL);
-	usList->head = (struct User*)malloc(sizeof(struct User));
+    assert(usList != NULL); 
+    usList->head = (struct User*)malloc(sizeof(struct User));
     assert(usList->head != NULL);
-	usList->head->next = NULL;
+    usList->head->next = NULL;
     usList->head->prev = NULL;
     str = "Kanye West";
     Start(&list, usList->head, 1, "");
-   	Start(&list, usList->head, 2, "");
- 	Start(&list, usList->head, 3, "");
+    Start(&list, usList->head, 2, "");
+    Start(&list, usList->head, 3, "");
     Start(&list, usList->head, 4, str);
     Report(&list, &usList);
     assert(usList->head->totalSum > 0);
-   	//ClearAlb(&list);
-	//free(list);
 }
 
 void FirstTest()
 {
 	struct AlbList* list = NULL;
-    char* str = (char*)malloc(sizeof(char) * 20);
+   	char* str = (char*)malloc(sizeof(char) * 20);
 	str = "Kanye West";
 	Alb(&list, str);
 	assert(list != NULL);
 	assert(list->head != NULL);
-    assert(list->tail != NULL);
-    assert(strncmp(list->head->perfID, "Kan", 3) == 0);
-    assert(strncmp(list->head->next->next->perfID, "Kil", 3) == 0);
-    assert(strncmp(list->head->next->next->next->next->perfID, "Emi", 3) == 0);
-    assert(strncmp(list->head->next->next->next->next->next->next->perfID, "Dav", 3) == 0);
+	assert(list->tail != NULL);
+	assert(strncmp(list->head->perfID, "Kan", 3) == 0);
+	assert(strncmp(list->head->next->next->perfID, "Kil", 3) == 0);
+	assert(strncmp(list->head->next->next->next->next->perfID, "Emi", 3) == 0);
+	assert(strncmp(list->head->next->next->next->next->next->next->perfID, "Dav", 3) == 0);
 }
 
 void AlbumsTest()
@@ -56,8 +54,6 @@ void AlbumsTest()
 	search = SearchByPerformer(&albs, "Avicii");
 	assert(search->head != NULL);
 	assert(strncmp(search->head->name, "TIM", 3) == 0);
-	//ClearAlb(&albs);
-	//assert(albs == NULL);
 }
 
 void PerfomancesTest()
@@ -70,10 +66,8 @@ void PerfomancesTest()
     assert(list != NULL);
     assert(list->head != NULL);
     assert(list->tail != NULL);
-	SearchID(&list, "-HHP");
-	SearchName(&list, gen);
-   	//PerfClear(&list);
-	//assert(list == NULL);
+    SearchID(&list, "-HHP");
+    SearchName(&list, gen);
 }
 
 void GenresTest()
@@ -87,8 +81,6 @@ void GenresTest()
 	GenInfo(&geners, 2);
 	GenInfo(&geners, 3);
 	GenInfo(&geners, 4);
-	//ClearAll(&geners);
-	//assert(geners == NULL);
 }
 
 void TestAll()
