c618702f7587a6681e8626624831f3f3f0a9bc53
nikolay777-ops <74809369+nikolay777-ops@users.noreply.github.com>
2021-06-01 03:53:54 +0300

    Update Test.c

--- Lab7Test/Test.c
+++ Lab7Test/Test.c
@@ -3,28 +3,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void UITest()
-{
-    struct AlbList* list = NULL;
-    struct UserList* usList = NULL;
-    char* str = (char*)malloc(sizeof(char) * 21);
-    assert(str != NULL);
-    AllAlbums(&list);
-    usList = (struct UserList*)malloc(sizeof(struct UserList));
-    assert(usList != NULL); 
-    usList->head = (struct User*)malloc(sizeof(struct User));
-    assert(usList->head != NULL);
-    usList->head->next = NULL;
-    usList->head->prev = NULL;
-    str = "Kanye West";
-    Start(&list, usList->head, 1, "");
-    //Start(&list, usList->head, 2, "");
-    //Start(&list, usList->head, 3, "");
-   // Start(&list, usList->head, 4, str);
-    //Report(&list, &usList);
-    assert(usList->head->totalSum > 0);
-}
-
 void FirstTest()
 {
 	struct AlbList* list = NULL;
@@ -88,7 +66,6 @@ void TestAll()
 	PerfomancesTest();
 	GenresTest();
 	AlbumsTest();
-	UITest();
 	printf("Tests are done!");
 }
 
